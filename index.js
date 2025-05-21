c7b0d9282e3db99b8e81e683bbd7986b8b4b527c
NguyenLuHoangSang <hoangsang.nguyen2001a@gmail.com>
2025-05-21 13:36:09 +0700

    get more information about IP

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 import express from "express";
-const app = express();
+import fetch from "node-fetch";
 
-app.get("/", (req, res) => {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("trust proxy", true);
+
+app.get("/", async (req, res) => {
 	const userIP =
 		req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
-	console.log(`Địa chỉ IP của người dùng: ${userIP}`);
-	res.send("Chào bạn! IP của bạn đã được ghi lại cho mục đích thống kê.");
+
+	try {
+		const geo = await fetch(`http://ip-api.com/json/${userIP}`);
+		const data = await geo.json();
+
+		console.log(`IP: ${userIP}`);
+		console.log("Location info:", data);
+
+		res.json({
+			ip: userIP,
+			country: data.country,
+			region: data.regionName,
+			city: data.city,
+			isp: data.isp,
+			lat: data.lat,
+			lon: data.lon,
+		});
+	} catch (err) {
+		console.error("Geo API failed", err);
+		res.status(500).send("Không thể truy xuất thông tin IP.");
+	}
 });
 
-app.listen(3000, () => {
-	console.log("Server đang chạy tại http://localhost:3000");
+app.listen(PORT, () => {
+	console.log(`Server đang chạy tại cổng ${PORT}`);
 });
